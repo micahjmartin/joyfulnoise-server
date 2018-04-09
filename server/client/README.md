8604ce19114f2068357b171400ad9958cd9990a9
Micah J. Martin <mjm5097@rit.edu>
2018-04-09 17:40:23 -0400

    Fixed readme, deleted client

--- server/client/README.md
+++
@@ -1,2 +0,0 @@
-# arsenal-api-python-client
-A client library for python that integrates with the Arsenal Teamserver.
--- server/client/__init__.py
+++
@@ -1,8 +0,0 @@
-#pylint: disable=invalid-name
-"""
-This package contains objects and methods for interacting with the Arsenal teamserver API.
-"""
-from .pyclient import Action, Group, GroupAction, Log, Session, Target
-from .pyclient import ArsenalClient, API_KEY_FILE, TEAMSERVER_URI
-
-from .cli import CLI
